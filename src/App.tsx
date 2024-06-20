3441893bc55e8505c1108f34e783736426c16f99
sufiyan <sp3596894@gmail.com>
2024-06-20 12:27:29 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='h-screen bg-[#101623] text-white flex justify-center'>
+      hello world
+    </div>
+  );
+}
+
+export default App;
