24736dbbad1355e000a104d3990a39e27ff8d1ed
sufiyan <sp3596894@gmail.com>
2024-06-20 13:14:25 +0530

    base architecture built

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
 
-function App() {
+
+export default function App() {
   return (
-    <div className='h-screen bg-[#101623] text-white flex justify-center'>
-      hello world
+    <div className='h-screen bg-[#101623] text-white flex flex-col'>
+      <div className="bg-[#2D3446]">navbar</div>
+      <div className="flex justify-between h-screen mt-1">
+        <div className="bg-[#1E2433] w-[16%] m-1 rounded">left col</div>
+        <div className="w-[61%] flex flex-col justify-between m-1">
+          <div className="bg-[#1E2433] h-[60%] mb-1 rounded">middle col upper</div> 
+          <div className="bg-[#1E2433] h-[40%] mt-1 rounded">middle col lower</div>
+        </div>
+        <div className="bg-[#1E2433] w-[23%] m-1 rounded">right col</div>
+      </div>
     </div>
   );
 }
-
-export default App;
