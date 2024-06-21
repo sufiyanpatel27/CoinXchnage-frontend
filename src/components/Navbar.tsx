438c3fded3579064608a4fdf8373f2b46fff1a42
sufiyan <sp3596894@gmail.com>
2024-06-21 19:01:51 +0530

    Navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,22 +8,22 @@ export default function Navbar({ mode, handleTheme }: { mode: boolean, handleThe
     return (
         <div className="bg-[#3067F0] dark:bg-[#2D3446] h-[45px] flex justify-between">
             <div className='flex items-center gap-4'>
-                <div className='ml-8 mr-4'>
+                <div className='ml-8 mr-4 w-36'>
                     <img src={logo} alt="Logo" className='' />
                 </div>
-                <div className='font-medium'>EXCHANGE</div>
-                <div className='font-medium'>P2P</div>
+                <div className='font-bold h-full w-[110px] bg-[#1F2531] flex justify-center items-center border-b-[3px] border-[#FAED9B]'>EXCHANGE</div>
+                <div className='font-bold'>P2P</div>
             </div>
             <div className='flex items-center gap-8'>
-                <div className='font-medium'>INVITE & EARN</div>
-                <div className='font-medium'>
+                <div className='font-semibold'>INVITE & EARN</div>
+                <div className='font-semibold'>
                     <img src={download} alt="Logo" className='w-5' />
                 </div>
-                <div className='font-medium cursor-pointer' onClick={() => handleTheme()}>
+                <div className='font-semibold cursor-pointer' onClick={() => handleTheme()}>
                     {!mode && <img src={theme_light} alt="Logo" className='w-5' />}
                     {mode && <img src={theme} alt="Logo" className='w-5' />}
                 </div>
-                <div className='font-medium mr-4 flex w-16 justify-between'>
+                <div className='font-semibold mr-4 flex w-16 justify-between'>
                     <img src={help} alt="Logo" className='w-5' />
                     HELP
                 </div>
