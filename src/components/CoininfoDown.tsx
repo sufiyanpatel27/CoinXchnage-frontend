293bcee157844f8ef412b585e7081b76160ad877
sufiyan <sp3596894@gmail.com>
2024-06-23 15:36:30 +0530

    home page structure comments added

--- src/components/CoininfoDown.tsx
+++ src/components/CoininfoDown.tsx
@@ -3,8 +3,8 @@
 export default function CoininfoDown() {
     return (
         <div className="h-[40%] mt-1 flex justify-between">
-            <div className="w-[63%] mr-1 bg-white dark:bg-[#1E2433] rounded"></div>
-            <div className="w-[37%] ml-1 bg-white dark:bg-[#1E2433] rounded"></div>
+            <div className="w-[63%] mr-1 bg-white dark:bg-[#1E2433] rounded flex justify-center items-center text-[#9EB1BF]">upcoming in phase II</div>
+            <div className="w-[37%] ml-1 bg-white dark:bg-[#1E2433] rounded flex justify-center items-center text-[#9EB1BF]">upcoming in phase II</div>
         </div>
     )
 }
