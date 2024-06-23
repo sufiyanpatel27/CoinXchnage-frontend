40060f543e28d04cd09acb5bfc0376e1f724f9f3
sufiyan <sp3596894@gmail.com>
2024-06-23 20:43:37 +0530

    phase I Ready

--- src/components/TradeInfo.tsx
+++ src/components/TradeInfo.tsx
@@ -3,8 +3,8 @@
 export default function TradeInfo() {
     return (
         <div className="w-[23%] mr-2 m-1 flex flex-col justify-between">
-            <div className="bg-white dark:bg-[#1E2433] mb-1 rounded h-[60%] flex justify-center items-center text-[#9EB1BF]">upcoming in phase II</div>
-            <div className="bg-white dark:bg-[#1E2433] mt-1 rounded h-[40%] flex justify-center items-center text-[#9EB1BF]">upcoming in phase II</div>
+            <div className="bg-white dark:bg-[#1E2433] mb-1 rounded h-[60%] flex justify-center items-center text-[#9EB1BF]">upcoming in phase III</div>
+            <div className="bg-white dark:bg-[#1E2433] mt-1 rounded h-[40%] flex justify-center items-center text-[#9EB1BF]">upcoming in phase III</div>
         </div>
     )
 }
